cb0e4fcfa156054ae4ab56a0cf55dce8197020ac
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-06 22:27:13 +0530

    initial project commit

---
+++ src/main/java/com/hardik/hadraniel/OtpGenerationValidationSpringBootPocApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.hadraniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpGenerationValidationSpringBootPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpGenerationValidationSpringBootPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/hadraniel/OtpGenerationValidationSpringBootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.hadraniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpGenerationValidationSpringBootPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
